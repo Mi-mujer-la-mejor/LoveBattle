9b25e12b34f943660d32aeddacdbbcc4ddc88906
Mi-mujer-la-mejor <samgongar519@gmail.com>
2026-05-30 03:00:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -544,7 +544,6 @@ btnMelannie.addEventListener("touchstart",(e)=> handleClick("melannie", btnMelan
     ctx.closePath(); ctx.fill(); ctx.restore();
   }
 
-  spawnFloatyHearts(event);
 
   for (let i = 0; i < COUNT; i++) {
     hearts.push({
